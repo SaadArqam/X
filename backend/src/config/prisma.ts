cf973e292b9c46bea860a68f7f6235e0f0a0ae53
Saad Arqam <saadarqamrnc@gmail.com>
2026-02-08 00:02:20 +0530

    fix: prisma client issue

--- backend/src/config/prisma.ts
+++ backend/src/config/prisma.ts
@@ -1,3 +1,11 @@
 import { PrismaClient } from "@prisma/client";
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { env } from "./env";
 
-export const prisma = new PrismaClient();
+const connectionString = `${env.DATABASE_URL}`;
+
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+
+export const prisma = new PrismaClient({ adapter });
