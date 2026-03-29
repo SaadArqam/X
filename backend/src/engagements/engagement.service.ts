69b7bb10d416dbdf4d58d7620484370f26cd6dbe
Saad Arqam <saadarqamrnc@gmail.com>
2026-03-30 00:35:36 +0530

    feat: added like and bookmark in schema + fs

---
+++ backend/src/engagements/engagement.service.ts
@@ -0,0 +1,9 @@
+import { prisma } from "../config/prisma";
+import { ApiError } from "../utils/ApiError";
+
+export const toggleLike(blogId, userId)=async(
+    blogId:number,
+    userId:number
+)=>{
+    
+}
